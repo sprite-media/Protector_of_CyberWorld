7f38a192269fb1fafe518b7102fb718cdc215424
Ding Zhang <36141388+Dgit35265@users.noreply.github.com>
2019-02-13 16:51:49 -0500

    implement player hands

---
+++ protector_of_cyberworld/Assets/Script/meshTurnOn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class meshTurnOn : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(TurnOn());
+    }
+        
+    IEnumerator TurnOn()
+    {
+
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+    }
+
+}
