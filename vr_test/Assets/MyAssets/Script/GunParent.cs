1568bee6547f526aa37d7315d93fba9ca1bc8d36
Orac92 <32988011+Orac92@users.noreply.github.com>
2019-02-16 19:34:42 -0500

    Converted to steam vr package
    Deleted the project with oculus package.
    Created a new project with steam vr package.
    gun grab and shoot.
    gun spawner each part.
    implemented teleport

---
+++ vr_test/Assets/MyAssets/Script/GunParent.cs
@@ -0,0 +1,51 @@
+﻿using Valve.VR;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+namespace Hyukin
+{
+    public class GunParent : MonoBehaviour
+    {
+        public int damage = 1;
+        public float range = 100f;
+
+        public float reFireTime = 0.15f;
+        protected float timer;
+
+        [SerializeField]private Hand hand;
+
+        public virtual void Update()
+        {
+            timer += Time.deltaTime;
+
+            //if (Input.GetButton("Fire1") && timer >= reFireTime) //for now
+            //{
+            //    Shoot();
+            //}
+            if (hand != null && hand.grabPinchAction.GetStateDown(hand.handType))
+            {
+                Shoot();
+            }
+        }
+
+        protected virtual void Shoot()
+        {
+            //Debug.Log("Bang!");
+            timer = 0;
+        }
+        public void Disappear()
+        {
+            Debug.Log("Disappear");
+            transform.parent.GetComponent<Rigidbody>().isKinematic = false;
+            hand = null;
+            //Particle effect
+            //Destroy(gameObject);
+        }
+        public void Grabbed()
+        {
+            hand = transform.parent.parent.GetComponent<Hand>();
+            transform.parent.GetComponent<Rigidbody>().useGravity = true;
+        }
+
+    }
+}
---
+++ vr_test/Assets/MyAssets/Script/RayGun.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Hyukin
+{
+    public class RayGun : GunParent
+    {
+        LineRenderer gunLine;
+        public float effectsDisplayTime = 0.2f; // how long lineRenderer will stay in a scene
+        int shootableMask; //enemy or should be shootable
+
+        private void Start()
+        {
+            gunLine = GetComponent<LineRenderer>();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if(timer >= reFireTime * effectsDisplayTime)
+            {
+                DisableEffects();
+            }
+        }
+
+        public void DisableEffects()
+        {
+            gunLine.enabled = false;
+        }
+
+        protected override void Shoot()
+        {
+            base.Shoot();
+
+            gunLine.enabled = true;
+            Transform shotTransform = transform; 
+            gunLine.SetPosition(0, shotTransform.position);
+            
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+            {
+
+                gunLine.SetPosition(1, hit.point);
+             
+                if (hit.transform.tag == "Enemy")
+                {
+                    //Debug.Log("takeDDDD");
+                    hit.transform.GetComponent<Virus1>().TakeDamage(damage);
+                }
+            }
+            else
+            {
+                gunLine.SetPosition(1, shotTransform.position + shotTransform.forward * range);
+            }
+        }
+    }
+}
