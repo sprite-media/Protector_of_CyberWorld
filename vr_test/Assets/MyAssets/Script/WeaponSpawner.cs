2976b50c882a6d9f95c611846e6ea5d941bfee14
Orac92 <32988011+Orac92@users.noreply.github.com>
2019-02-21 08:34:29 -0500

    Replace project
    due to change to steam vr

---
+++ vr_test/Assets/MyAssets/Script/WeaponSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public enum WeaponType
+    {
+        LeftGun,
+        RightGun,
+        Sword,
+        Count
+    };
+    public WeaponType weaponType = WeaponType.LeftGun;
+
+    private static GameObject[] weapon = null;
+
+    private GameObject spawnedWeapon = null;
+
+    private const float spawnCoolTime = 2.0f;
+    private float spawnTimer = 0.0f;
+
+    private void Awake()
+    {
+        if (weapon == null)
+        {
+            weapon = new GameObject[(int)WeaponType.Count];
+            
+            weapon[0] = Resources.Load("LeftGun", typeof(GameObject)) as GameObject;
+            weapon[1] = Resources.Load("RightGun", typeof(GameObject)) as GameObject;
+            //weapon[2] = Resources.Load("LeftGun", typeof(GameObject)) as GameObject;
+        }
+    }
+
+    void Update()
+    {
+        if(spawnedWeapon == null)
+        {
+            spawnTimer += Time.deltaTime;
+
+            if(spawnTimer >= spawnCoolTime)
+            {
+                spawnedWeapon = (GameObject)Instantiate(weapon[(int)weaponType], transform.position, transform.rotation);
+                spawnedWeapon.transform.parent = transform;
+                spawnTimer = 0.0f;
+            }
+        }
+
+        if (transform.childCount == 0)
+            spawnedWeapon = null;
+    }
+}
