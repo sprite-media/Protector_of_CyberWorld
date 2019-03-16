5ee7587c4662877537a51460c97763ee551cb31e
Orac92 <32988011+Orac92@users.noreply.github.com>
2019-03-16 18:06:05 -0400

    Added LaserCharging particle

--- vr_test/Assets/MyAssets/Script/Player/RayGun.cs
+++ vr_test/Assets/MyAssets/Script/Player/RayGun.cs
@@ -1,105 +1,119 @@
 ﻿using UnityEngine;
 
-namespace Hyukin
+public class RayGun : GunParent
 {
-    public class RayGun : GunParent
+    public LineRenderer gunLine;
+
+    public float effectsDisplayTime = 0.2f; // how long lineRenderer will stay in a scene
+    int shootableMask; //enemy or should be shootable
+    public AudioSource aud;
+
+    private static GameObject hitParticle = null;
+    private static GameObject flashParticle = null;
+    private static GameObject ChargedLaser = null;
+    private static GameObject ChargingLaserParticle = null;
+
+
+    private void Start()
     {
-        public LineRenderer gunLine;
+        //gunLine = GetComponent<LineRenderer>();
+        if (hitParticle == null)
+            hitParticle = Resources.Load("Particle_LaserHit", typeof(GameObject)) as GameObject;
 
-        public float effectsDisplayTime = 0.2f; // how long lineRenderer will stay in a scene
-        int shootableMask; //enemy or should be shootable
-        public AudioSource aud;
+        if (flashParticle == null)
+            flashParticle = Resources.Load("Particle_LaserFlash", typeof(GameObject)) as GameObject;
 
-		private static GameObject hitParticle = null;
-        private static GameObject flashParticle = null;
+        if (ChargedLaser == null)
+            ChargedLaser = Resources.Load("GO_ChargedLaser", typeof(GameObject)) as GameObject;
 
+        if (ChargingLaserParticle == null)
+            ChargingLaserParticle = Resources.Load("Particle_LaserCharging", typeof(GameObject)) as GameObject;
+    }
 
-
-        private void Start()
+    public override void Update()
+    {
+        base.Update();
+        if (timer >= reFireTime * effectsDisplayTime)
         {
-            //gunLine = GetComponent<LineRenderer>();
-			if(hitParticle == null)
-                hitParticle = Resources.Load("Particle_LaserHit", typeof(GameObject)) as GameObject;
-
-            if (flashParticle == null)
-                flashParticle = Resources.Load("Particle_LaserFlash", typeof(GameObject)) as GameObject;
+            DisableEffects();
         }
 
-        public override void Update()
+        if (isCharging)
         {
-            base.Update();
-            if(timer >= reFireTime * effectsDisplayTime)
-            {
-                DisableEffects();
-            }
+            chargingLaser += Time.deltaTime;
+            GameObject goCharingLaserParticle = (GameObject)Instantiate(ChargingLaserParticle, transform.position, transform.rotation);
+            Destroy(goCharingLaserParticle, 0.1f);
+        }
+        else
+            chargingLaser = 0.0f;
+    }
+
+    public void DisableEffects()
+    {
+        gunLine.enabled = false;
+    }
+
+    protected override void Shoot()
+    {
+        base.Shoot();
+
+
+        if (chargingLaser < minChargingLaser && isCharging)
+        {
+            NormalShoot();
         }
 
-        public void DisableEffects()
+        if (chargingLaser > minChargingLaser && isCharging)
         {
-            gunLine.enabled = false;
+            ChargedLaserShoot();
         }
-
-        protected override void Shoot()
-        {
-            base.Shoot();
-
-
-            if (chargingLaser < minChargingLaser && !isCharging)
-            {
-                NormalShoot();
-                chargingLaser = 0.0f;
-            }
-            
-            if(chargingLaser > minChargingLaser && !isCharging)
-            {
-                Debug.Log("Charged Laser Shoot");
-                chargingLaser = 0.0f;
-            }
-          
-        }
-
-        private void ChargingLaser()
-        {
-
-        }
-
-        private void NormalShoot()
-        {
-            GameObject goFlash = (GameObject)Instantiate(flashParticle, transform.position, transform.rotation);
-            Destroy(goFlash, 0.22f);
-            aud.Play();
-            gunLine.enabled = true;
-            Transform shotTransform = transform;
-            gunLine.SetPosition(0, shotTransform.position);
-
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, range))
-            {
-                gunLine.SetPosition(1, hit.point);
-                InstantiateParticle(hit);
-
-                if (hit.transform.tag == "Enemy")
-                {
-                    hit.transform.GetComponent<Enemy>().TakeDamage(damage);
-                }
-                else if (hit.transform.tag == "Boss")
-                {
-                    hit.transform.GetComponent<Enemy>().TakeDamage(damage);
-                }
-            }
-            else
-            {
-                gunLine.SetPosition(1, shotTransform.position + shotTransform.forward * range);
-            }
-        }
-
-		private void InstantiateParticle(RaycastHit hit)
-		{
-			GameObject tempParticle = (GameObject)Instantiate(hitParticle, hit.point, hit.transform.rotation);
-			tempParticle.transform.LookAt(transform, Vector3.up);
-			tempParticle.SetActive(true);
-			Destroy(tempParticle, 1.4f);
-		}
 
     }
+
+    private void ChargedLaserShoot()
+    {
+        GameObject goChargedLaser = (GameObject)Instantiate(ChargedLaser, transform.position, transform.rotation);
+    }
+
+    private void NormalShoot()
+    {
+        //FlashLight
+        GameObject goFlash = (GameObject)Instantiate(flashParticle, transform.position, transform.rotation);
+        Destroy(goFlash, 0.22f);
+
+
+        aud.Play();
+        gunLine.enabled = true;
+        Transform shotTransform = transform;
+        gunLine.SetPosition(0, shotTransform.position);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            gunLine.SetPosition(1, hit.point);
+            InstantiateParticle(hit);
+
+            if (hit.transform.tag == "Enemy")
+            {
+                hit.transform.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            else if (hit.transform.tag == "Boss")
+            {
+                hit.transform.GetComponent<Enemy>().TakeDamage(damage);
+            }
+        }
+        else
+        {
+            gunLine.SetPosition(1, shotTransform.position + shotTransform.forward * range);
+        }
+    }
+
+    private void InstantiateParticle(RaycastHit hit)
+    {
+        GameObject tempParticle = (GameObject)Instantiate(hitParticle, hit.point, hit.transform.rotation);
+        tempParticle.transform.LookAt(transform, Vector3.up);
+        tempParticle.SetActive(true);
+        Destroy(tempParticle, 1.4f);
+    }
+
 }
