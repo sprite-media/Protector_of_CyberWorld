636e6b7eb236b61d098634231119067a3ebbbd40
Ding Zhang <36141388+Dgit35265@users.noreply.github.com>
2019-03-27 20:05:37 -0400

    Tutorial_Map

---
+++ vr_test/Assets/MyAssets/Script/SimpleRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    public enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+    public Axis AxisToRotate = Axis.X;
+    public float speed;
+
+
+    public void Update()
+    {
+        switch ((int)AxisToRotate)
+        {
+            case (int)Axis.X:
+                transform.Rotate(speed * Time.deltaTime, 0, 0);
+                break;
+            case (int)Axis.Y:
+                transform.Rotate(0, speed * Time.deltaTime, 0);
+                break;
+            case (int)Axis.Z:
+                transform.Rotate(0, 0, speed * Time.deltaTime);
+                break;
+        }
+    }
+}
---
+++ vr_test/Assets/MyAssets/Script/Tutorial/Hologram.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Hologram : MonoBehaviour
+{
+    [SerializeField] GameObject[] viruses;
+    int curIdx = 0;
+
+
+    float changeTime =  48.0f;
+    float curChangeTime = 0.0f;
+
+    private void Start()
+    {
+        ChangeDisplay();
+    }
+
+    private void Update()
+    {
+        curChangeTime += 0.1f;
+        if(curChangeTime >= changeTime)
+        {
+            curChangeTime = 0.0f;
+
+            if (curIdx + 1 < viruses.Length)
+                curIdx += 1;
+            else
+                curIdx = 0;
+
+            ChangeDisplay();
+        }
+    }
+
+
+    private void ChangeDisplay()
+    {
+        for (int i = 0; i < viruses.Length; i++)
+        {
+            viruses[i].SetActive(false);
+        }
+        viruses[curIdx].SetActive(true);
+    }
+
+}
